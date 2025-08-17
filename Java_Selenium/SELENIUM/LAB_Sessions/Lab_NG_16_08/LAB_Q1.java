8f79e1f61a42f423df2c60386f03c4db03af63a2
Saswatpati06 <143920400+Saswatpati06@users.noreply.github.com>
2025-08-17 22:48:17 +0530

    Add files via upload

---
+++ Java_Selenium/SELENIUM/LAB_Sessions/Lab_NG_16_08/LAB_Q1.java
@@ -0,0 +1,24 @@
+package Labs_NG_16_08;
+
+
+
+import org.testng.annotations.Test;
+
+public class LAB_Q1 {
+
+    @Test(groups = "Smoke")
+    public void loginTest() {
+        System.out.println("Login Test executed");
+    }
+
+    @Test(groups = "Regression")
+    public void paymentTest() {
+        System.out.println("Payment Test executed");
+    }
+
+    @Test(groups = {"Smoke", "Regression"})
+    public void logoutTest() {
+        System.out.println("Logout Test executed");
+    }
+}
+
